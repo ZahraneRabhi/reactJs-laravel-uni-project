3ad19984a25c9d7bc8bf31fd03de0ac57f65ab38
Zahrane <zahranerabhi@outlook.com>
2024-11-29 10:00:42 +0100

    Implements Frontend

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\CategorieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/categories', [CategorieController::class, 'index']);
+Route::get('/{any}', function () {
+    return view('welcome');
+    })->where('any', '.*');
