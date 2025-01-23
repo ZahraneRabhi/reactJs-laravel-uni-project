a4f6317c95881e8ee32cfd22934566b983d77991
Zahrane <zahranerabhi@outlook.com>
2025-01-23 12:58:00 +0100

    deploy: deploy frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Mon application Laravel</title>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-    <link href="{{ secure_asset('frontend/assets/index-DnVUw_vK.css') }}" rel="stylesheet">
+<head>
+    <title>Mon Projet Laravel-ReactTS</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link href="{{ secure_asset('frontend/assets/index-CZpCW9iA.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="root"></div>
-    <script src="{{ secure_asset('frontend/assets/index-DlLDauy7.js')}}"></script>
+    <script src="{{ secure_asset('frontend/assets/index-CvTbDyNd.js')}}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
-
-use App\Http\Controllers\CategorieController;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/categories', [CategorieController::class, 'index']);
 Route::get('/{any}', function () {
-    return view('welcome');
-    })->where('any', '.*');
+return view('welcome');
+})->where('any', '.*');
